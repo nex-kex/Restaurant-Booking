87e605c35ecdab0273f0a8bb0d35753a3ac18ce2
Mark Volkov <markasch67@gmail.com>
2025-09-01 22:20:56 +0300

    Fixed templates

--- booking/admin.py
+++ booking/admin.py
@@ -5,18 +5,18 @@ from .models import Booking, Category, Table
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ["name"]
+    list_display = ["id", "name"]
     search_fields = ["name"]
 
 
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
-    list_display = ["price", "seats", "category", "is_available"]
+    list_display = ["id", "price", "seats", "category", "is_available"]
     list_filter = ["category", "is_available"]
 
 
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ["user", "table", "date", "status"]
+    list_display = ["id", "user", "table", "date", "status"]
     search_fields = ["user"]
     list_filter = ["status", "table"]
--- users/admin.py
+++ users/admin.py
@@ -5,6 +5,6 @@ from users.models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ["phone_number", "email", "first_name", "last_name"]
+    list_display = ["id", "phone_number", "email", "first_name", "last_name"]
     ordering = ["first_name", "last_name"]
     search_fields = ["phone_number", "email", "first_name", "last_name"]
